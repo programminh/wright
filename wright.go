7615bad0a08a29227db0e819d7bfbf8b9f4559fd
Truong Pham <Truong Pham>
2017-03-16 00:49:22 -0400

    Add slack message

--- wright.go
+++ wright.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/programminh/wright/qpx"
@@ -15,23 +20,69 @@ type Trip interface {
 	Carrier() string
 }
 
+var green = "#66BB6A"
+var darkgreen = "#1B5E20"
+
 func main() {
 	var (
-		trip Trip
-		err  error
+		edi, gla Trip
+		err      error
+		date     = "2017-05-19"
+		dst      = "YUL"
 	)
 
-	fmt.Println("Finding cheapest flight from EDI to YUL on 2017-05-19")
-	if trip, err = qpx.Cheapest("EDI", "YUL", "2017-05-19"); err != nil {
+	if edi, err = qpx.Cheapest("EDI", dst, date); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Printf("Found %.2f$ - takes %s with %d planes by %s\n", trip.Price(), trip.Duration(), trip.Stops(), trip.Carrier())
+	slack("EDI", dst, date, green, edi)
 
-	fmt.Println("Finding cheapest flight from GLA to YUL on 2017-05-19")
-	if trip, err = qpx.Cheapest("GLA", "YUL", "2017-05-19"); err != nil {
+	if gla, err = qpx.Cheapest("GLA", dst, date); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	fmt.Printf("Found %.2f$ - takes %s with %d planes by %s\n", trip.Price(), trip.Duration(), trip.Stops(), trip.Carrier())
+	slack("GLA", dst, date, darkgreen, gla)
+}
+
+type message struct {
+	Username    string       `json:"username"`
+	Channel     string       `json:"channel"`
+	IconEmoji   string       `json:"icon_emoji"`
+	Attachments []attachment `json:"attachments"`
+}
+
+type attachment struct {
+	Fallback   string   `json:"fallback"`
+	Title      string   `json:"title"`
+	Text       string   `json:"text"`
+	Color      string   `json:"color"`
+	Carrier    string   `json:"carrier"`
+	Footer     string   `json:"footer"`
+	MarkdownIn []string `json:"mrkdwn_in"`
+}
+
+func slack(origin, destination, date, color string, trip Trip) {
+	msg := message{
+		Username:  "Wright",
+		Channel:   "@sillybruce",
+		IconEmoji: ":google:",
+	}
+
+	att := attachment{
+		Color:    color,
+		Fallback: fmt.Sprintf("Cheapest flight from %s to %s on %s", origin, destination, date),
+		Title:    fmt.Sprintf("%s to %s on %s - %.2f$", origin, destination, date, trip.Price()),
+		Text:     fmt.Sprintf("Duration %s with %d stops", strings.TrimSuffix(fmt.Sprintf("%s", trip.Duration()), "m0s"), trip.Stops()),
+		Footer:   fmt.Sprintf("by %s", trip.Carrier()),
+	}
+
+	msg.Attachments = []attachment{att}
+
+	buf := bytes.NewBuffer(nil)
+
+	json.NewEncoder(buf).Encode(msg)
+
+	if _, err := http.Post(endpoint, "application/json", buf); err != nil {
+		log.Println(err)
+	}
 }
